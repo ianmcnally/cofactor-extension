8ed04fb1fbc3840e3368c4c37e45b2794cbd068c
Ian McNally <imcnally@amplify.com>
2014-06-24 21:30:59 -0400

    make it work; icons; styling

--- application.js
+++ application.js
@@ -1,1 +1,5 @@
-console.log('hey');
+var tabList = document.getElementsByClassName('tab-links')[0]
+tabList.addEventListener('click', function(event){
+  item = event.target;
+  chrome.windows.create({url : item.dataset.tabhref});
+});
